ea7b2ce6d7b4dcab0632298b293e6fd852dfb975
twelveman <18823207889@139.com>
2020-07-21 12:50:38 +0800

    第一次提交

---
+++ src/main/java/com/newxton/companywebsite/CompanywebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.newxton.companywebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanywebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanywebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/newxton/companywebsite/CompanywebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newxton.companywebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanywebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
