a27bc48ff40a0769e5685120a1a0a8649e35a103
twelveman <18823207889@139.com>
2020-08-04 21:18:26 +0800

    bug fix

--- src/main/java/com/newxton/companywebsite/controller/web/NxtAdminController.java
+++ src/main/java/com/newxton/companywebsite/controller/web/NxtAdminController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author soyojo.earth@gmail.com
  * @time 2020/7/21
@@ -19,15 +21,35 @@ public class NxtAdminController {
      * @return
      */
     @RequestMapping("/admin")
-    public ModelAndView index(ModelAndView model) {
+    public ModelAndView index(ModelAndView model, HttpServletRequest request) {
 
         //这里直接加载前端工程师的打包页面即可，没其他啥事
-        //model.setViewName("admin");
 
-        model.setViewName("admin");
+        if (isMSBrowser(request)){
+            model.setViewName("admin_no_ie");
+        }
+        else {
+            model.setViewName("admin");
+        }
 
         return model;
 
+    }
+
+    /**
+     * 	判断是不是ie浏览器
+     * @param request
+     * @return
+     */
+    public boolean isMSBrowser(HttpServletRequest request) {
+        String[] IEBrowserSignals = {"MSIE", "Trident", "Edge"};
+        String userAgent = request.getHeader("User-Agent");
+        for (String signal : IEBrowserSignals) {
+            if (userAgent.contains(signal)){
+                return true;
+            }
+        }
+        return false;
     }
 
 }
